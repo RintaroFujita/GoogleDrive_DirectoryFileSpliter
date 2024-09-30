12b5cead5095833d2dbdd76124bc6cca395e7fdd
Rintaro Fujita <rinpc356@gmail.com>
2024-10-01 05:10:32 +0900

    update

--- filespliter.py
+++ filespliter.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-source_dir = '/content/drive/MyDrive/I2T Recognition Dataset/ships_images'
+src_dir = '/content/drive/MyDrive/I2T Recognition Dataset/ships_images'
 filesplit_dirs = [
     '/content/drive/MyDrive/I2T Recognition Dataset/ships_images/1',
     '/content/drive/MyDrive/I2T Recognition Dataset/ships_images/2',
@@ -18,10 +18,10 @@ dir_num = 0
 
 for image_file in image_files:
     if image_file.endswith(('.png', '.jpg', '.jpeg')):
-        src_path = os.path.join(source_dir, image_file)
+        src = os.path.join(src_dir, image_file)
         detect_dir = filesplit_dirs[dir_num]
         file_path = os.path.join(detect_dir, image_file)
-        shutil.move(src_path, file_path)
+        shutil.move(src, file_path)
         dir_num += 1
         if dir_num >= len(filesplit_dirs):
             dir_num = 0
